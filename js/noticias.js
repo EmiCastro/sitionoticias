fdd133267842864a229bea887985c9c8aa07fab8
Emi Castro <emicastro1993@gmail.com>
2017-08-23 20:37:32 -0300

    se borraron funciones innecesarias que estaban comentadas en el código

--- js/noticias.js
+++ js/noticias.js
@@ -14,27 +14,6 @@
 
 var noticias = obtenerNoticias();
 
-// Esta función está de muestra para ver como funciona el while
-// Sólo se está recorriendo las noticias y cambiándole el color
-/*function marcarNoticiasConWhile(){
-  var contador = 0;
-  // Recorre la variable noticias, iluminando en la que se encuentra en cada momento
-  while(contador < noticias.length){
-    noticiaActual = noticias[contador];
-    cambiarColor(noticiaActual, 'rgb(188, 164, 213)');
-    contador++; //contador = contador + 1
-  }
-}
-*/
-// Ahora lo mismo pero con un for para ver cuales son las diferencias con el while
-
-/*function marcarNoticiasConFor(){
-  for(var contador = 0; contador<noticias.length; contador++){
-    noticiaActual = noticias[contador];
-    cambiarColor(noticiaActual, 'rgb(235, 190, 162)');
-  }
-}
-*/
 
 // Esta la tienen que hacer, puede ser con while o for
 function resaltarNoticiasQueContengan(palabra, color){
@@ -85,8 +64,3 @@ function recortarNoticias(cantPalabras){
 
 // En este caso el parámetro son 20 palabras.
 recortarNoticias(20);
-
-// Acá abajo podés poner las funciones que quieras usar
-//marcarNoticiasConWhile()
-//marcarNoticiasConFor()
-// ...
